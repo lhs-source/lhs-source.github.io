142a25acd989e814235575263b5baf2ec6abedfd
hyunsoolee <iddms5446@gmail.com>
2022-06-25 01:01:11 +0900

    prerender-spa-plugin 이슈 해결 + 설정

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,10 @@
-const path = require('path')
-const PrerenderSpaPlugin = require('prerender-spa-plugin')
-const posts = require('./public/posts/postlist.json')
-const routes = posts.map(post => `/${post.url}`)
-
-console.log('routes', routes);
-
-const postPlugins = [
-  new PrerenderSpaPlugin({
-    staticDir: path.join(__dirname, 'dist'), // docs: output 디렉토리 명
-    routes: routes,
-    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
-      renderAfterElementExists: '#app'
-    }),
-  }),
-];
+const webpackPlugins = require('./webpack.plugin');
 
 module.exports = {
+  outputDir: 'dist',
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(...postPlugins); // 상단에서 정의한 postPlugins 내용 삽입
+      config.plugins.push(...webpackPlugins); // 상단에서 정의한 postPlugins 내용 삽입
     }
   }
 }
