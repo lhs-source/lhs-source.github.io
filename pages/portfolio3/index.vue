cc9e2cbf2f0d714585a6b616fade219536e81213
lhs-source <iddms5446@gmail.com>
2025-11-21 09:43:18 +0900

    새로운 포트폴리오 또 팜

---
+++ pages/portfolio3/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="portfolio3-wrapper">
+    <div class="main-background__resume-list">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.portfolio3-wrapper {
+  width: 100vw;
+  height: 100vh;
+}
+
+.main-background__resume-list {
+  width: 100%;
+  height: 100%;
+  /* background-color: white; */
+  background: url('https://blog.kakaocdn.net/dna/bwpmFU/btsP3eOfPHO/AAAAAAAAAAAAAAAAAAAAAG_g7Pf8IJuIcI_oYMNCZLKgUNKw_Y2d5UxlGwGD_jeZ/img.png?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1764514799&allow_ip=&allow_referer=&signature=X3Xxsm%2FBzrLRWaEHKSNkWo6kX5Y%3D') no-repeat center center;
+}
+</style>
